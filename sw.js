4cc18fa8e7e95d63c23ca0ac06b1c197257dd201
Jscott235 <116557786+Jscott235@users.noreply.github.com>
2026-08-01 23:19:18 +0100

    SW v2: no caching, always network

--- sw.js
+++ sw.js
@@ -1,14 +1,4 @@
-// v5 - no caching, always network
-const CACHES_TO_DELETE = ['dancing-cup-v1','dancing-cup-v2','dancing-cup-v3','dancing-cup-v4'];
-self.addEventListener('install', e => { self.skipWaiting(); });
-self.addEventListener('activate', e => {
-  e.waitUntil(
-    caches.keys()
-      .then(keys => Promise.all(keys.map(k => caches.delete(k))))
-      .then(() => clients.claim())
-  );
-});
-self.addEventListener('fetch', e => {
-  // Always go to network, no caching
-  e.respondWith(fetch(e.request));
-});
+const v='lsv2';
+self.addEventListener('install',()=>self.skipWaiting());
+self.addEventListener('activate',e=>{e.waitUntil(caches.keys().then(ks=>Promise.all(ks.map(k=>caches.delete(k)))).then(()=>clients.claim()))});
+self.addEventListener('fetch',e=>e.respondWith(fetch(e.request)));
