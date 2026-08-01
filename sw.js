eb5fec76ce984e2e09d4bc99a6d9658c8e81e5b1
Jscott235 <116557786+Jscott235@users.noreply.github.com>
2026-08-01 23:22:59 +0100

    SW killer: unregister self and clear all caches

--- sw.js
+++ sw.js
@@ -1,4 +1,12 @@
-const v='lsv2';
-self.addEventListener('install',()=>self.skipWaiting());
-self.addEventListener('activate',e=>{e.waitUntil(caches.keys().then(ks=>Promise.all(ks.map(k=>caches.delete(k)))).then(()=>clients.claim()))});
-self.addEventListener('fetch',e=>e.respondWith(fetch(e.request)));
+
+// Unregister self and clear everything
+self.addEventListener('install', () => self.skipWaiting());
+self.addEventListener('activate', e => {
+  e.waitUntil(
+    caches.keys()
+      .then(keys => Promise.all(keys.map(k => caches.delete(k))))
+      .then(() => self.registration.unregister())
+      .then(() => clients.matchAll())
+      .then(clients => clients.forEach(c => c.navigate(c.url)))
+  );
+});
